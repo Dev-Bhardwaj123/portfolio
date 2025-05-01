b6bcf26ef2408a9a88a6af8631bfb3c311b8fbdd
Dev <bhardwajdev456@gmail.com>
2025-05-01 21:58:55 +0530

    init

---
+++ components/CodingProfiles.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Button } from "./ui/MovingBorders";
+
+const codingProfiles = [
+  {
+    id: 1,
+    name: "LeetCode",
+    url: "https://leetcode.com/u/Dev_Bhardwaj123/",
+    logo: "/leetcode-logo.svg",
+    description: "Solved over 700 questions with a streak of 100+ days.",
+  },
+  {
+    id: 2,
+    name: "GeeksforGeeks",
+    url: "https://www.geeksforgeeks.org/user/bhardwajlvxp/",
+    logo: "/gfg-logo.svg",
+    description: "Solved 100+ questions.",
+  },
+  {
+    id: 3,
+    name: "CodeForces",
+    url: "https://codeforces.com/profile/devBhardwaj69",
+    logo: "/codeforces.jpg",
+    // logo: "/codechef.jfif",
+    description: "Max contest Rating 900+ .",
+  },
+];
+
+const CodingProfiles = () => {
+  return (
+    <div className="py-20 w-full" id="code">
+      <h1 className="heading text-center text-4xl font-semibold text-gray-900 dark:text-white mb-8">
+        My <span className="text-purple">Coding Profiles</span>
+      </h1>
+
+      <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-8 mx-auto max-w-5xl">
+        {codingProfiles.map((profile) => (
+          <Button
+            key={profile.id}
+            duration={Math.floor(Math.random() * 10000) + 10000} // Random animation duration
+            borderRadius="1.75rem"
+            style={{
+              background: "rgb(4,7,29)",
+              backgroundColor:
+                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              borderRadius: `calc(1.75rem * 0.96)`,
+            }}
+            className="relative text-black dark:text-white border-neutral-200 dark:border-slate-800 flex flex-col items-center p-6 transition-transform transform hover:scale-105 shadow-lg rounded-xl overflow-hidden"
+          >
+            <a
+              href={profile.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center"
+            >
+              <img
+                src={profile.logo}
+                alt={`${profile.name} logo`}
+                className="w-20 h-20 mb-4"
+              />
+              <h2 className="text-2xl font-semibold mb-2">{profile.name}</h2>
+              <p className="text-sm text-gray-300 text-center px-4">
+                {profile.description}
+              </p>
+            </a>
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CodingProfiles;
---
+++ data/index.ts
@@ -0,0 +1,267 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Coding Profiles", link: "#code" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development and AI/ML.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working on a Deep CNN image classifier to detect emotions",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+       
+    {
+      id: 1,
+      title: "MoodDetector",
+      des: "A Deep CNN image classifier that introspects whether the person in image is happy or not.",
+      img: "/deepCnn.jpg",
+      iconLists: ["jupyter.png","Gc.png","/python.jpeg","/stream.svg"],
+      link: "https://moodanalyzer.streamlit.app/",
+    },
+  {
+      id: 2,
+      title: "SolveSudoku",
+      des: "A Sudoku Solver Visualizer with options to change speed and visualize buttons.",
+      img: "/snapy.png",
+      iconLists: ["java.png","/js.png","/html.png", "/css.jpeg"],
+      link: "https://github.com/Dev-Bhardwaj123/Sudoku-Solver-Visualizer",
+    }, 
+
+    {
+      id: 4,
+      title: "PlaySync",
+      des: "An automated spotify terraform playlist to minimize human intervention and automate actions.",
+      img: "/spProj.jpg",
+      iconLists: ["/docker.png","/terraform.png","/spotify.png","/HCL.png"],
+      link: "https://github.com/Dev-Bhardwaj123/Spotify-Terraform-Playlist",
+    },
+    {
+      id: 5,
+      title: "LC Questions",
+      des: "A detailed collections of good LeetCode questions with solutions for DSA and CP",
+      img: "/leetcodelogo.jpg",
+      iconLists: ["/leetcode-logo.svg", ,"/python.jpeg","/cpp.png","/c.svg"],
+      link: "https://github.com/Dev-Bhardwaj123/LC-Questions",
+    },     
+    {
+      id: 6,
+      title: "HierarLock",
+      des: "A Hierarchical Locking system that helps maintain functionalities with specific rules.",
+      img: "/jus.jpg",
+      iconLists: ["/cpp.png","/c.svg"],
+      link: "https://github.com/Dev-Bhardwaj123/Hierarchical-Locking-System"
+    }, 
+
+    {
+      id: 3,
+      title: "Movie Recc",
+      des: "A movie recommendation system that using ML algorithms to suggest 10 movies similar to user input one.",
+      img: "/movie.jpg",
+      iconLists: ["/python.jpeg","/excl.png","/html.png", "/css.jpeg","/js.png"],
+      link: "https://github.com/Dev-Bhardwaj123/Movie-Recommendation-system",
+    },
+  ];
+  
+
+  export const articles = [
+    {
+      id: 1,
+      img: "/oneshotarticle.png",
+      title: "One-Shot Interview Prep Guide",
+      des:
+        "A quick and focused revision guide covering DSA, System Design, DBMS, OS, CN & more for last-minute interview prep.",
+      link: "https://www.linkedin.com/pulse/your-one-stop-revision-resource-cracking-software-roles-bhardwaj-zqbgc/",
+      iconLists: ["/linkedin.jpeg"],
+    },
+    {
+      id: 2,
+      img: "/microarticle.png",
+      title: "How I Cracked the Microsoft Internship",
+      des:
+        "Step-by-step breakdown of my Microsoft interview process, preparation strategy, and key takeaways.",
+      link: "https://leetcode.com/discuss/post/5857898/microsoft-swe-intern-hyderabad-bengaluru-dofg/",
+      iconLists: ["/leetcode-logo.svg"],
+    },
+    {
+      id: 3,
+      img: "/dsaarticle.png",
+      title: "Top DSA Sheets to Follow",
+      des:
+        "A curated list of the most effective DSA question sheets for cracking MAANG/FAANG and top tech interviews — handpicked with explanations.",
+      link: "https://www.linkedin.com/pulse/top-dsa-sheets-follow-tushar-bhardwaj--vigec/",
+      iconLists: ["/linkedin.jpeg","/leetcode-logo.svg"],
+    }
+    
+  ];
+
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Website Developer",
+      desc: "Working on client projects, developing websites and bots that enhanced user experience and functionality by designing intuitive, user-friendly interfaces.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+    {
+      id: 3,
+      title: "Operations Interns",
+      desc: "Managing daily operations of cohort-based programs by optimizing processes, analyzing performance data, and collaborating with leadership to enhance program efficiency and participant support.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 2,
+      title: "Content Problem Reviewer and Solution Author - Leetcode",
+      desc: "Oversaw a quality control initiative for LeetCode problems, revising 120 problem statements and examples.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    
+    {
+      id: 2,
+      title: "Technical Head - Apna College LPU",
+      desc: "Directing the technical team in developing and implementing innovative solutions for the Apna College LPU club.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/Dev-Bhardwaj123",
+    },
+    {
+      id: 2,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/bhardwajdev2003/",
+    },
+  ];
